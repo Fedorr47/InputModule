19c0749d2fae52b1782bfdbbd51c8652838af913
Федор Андреев <fedorr47@gmail.com>
2024-06-24 12:05:52 +0200

    A little bit reworking

--- InputModule.h
+++ InputModule.h
@@ -9,12 +9,14 @@ namespace InputModule
     private:
         struct Private { explicit Private() = default; };
 
+        InputManager() = default;
+
         static inline std::once_flag init_flag_;
         static inline std::unique_ptr<InputManager> mng_instance_{ nullptr };
     public:
         InputManager(const InputManager&) = delete;
         InputManager(InputManager&&) = delete;
-        InputManager(Private) {};
+        InputManager(Private) {}
         ~InputManager() = default;
 
         InputManager& operator=(const InputManager&) = delete;
--- TestEventModule/InputModule.cpp
+++ TestEventModule/InputModule.cpp
@@ -1,18 +1,17 @@
 #include "gtest/gtest.h"
 #include "InputModule.h"
 
-TEST(InputModule, CreationAndDestuction)
+TEST(InputModule, Creation)
 {
   using namespace InputModule;
    {
      auto* manager_1 = InputManager::get_instance();
-     manager_1->set_a(10);
      EXPECT_TRUE(manager_1 != nullptr);
    }
    const auto* manager_2 = InputManager::get_instance();
    EXPECT_TRUE(manager_2 != nullptr);
    const auto* manager_3 = manager_2;
-   EXPECT_FALSE(manager_2 == nullptr);
+   EXPECT_FALSE(manager_3 == nullptr);
 }
 
 TEST(InputModule, Initialization)
