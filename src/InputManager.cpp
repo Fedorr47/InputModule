60a5cf955ab484fad2f4adf8940767d4598941d3
Федор Андреев <fedorr47@gmail.com>
2024-06-24 12:36:43 +0200

    Refactoring

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -4,12 +4,20 @@ namespace InputModule
 {
     InputManager* InputManager::get_instance()
     {
-        std::call_once(init_flag_, []()
-            {
-                mng_instance_ = std::make_unique<InputManager>(Private());
-            }
-        );
-        
-        return mng_instance_.get();
+        try
+        {
+            // Create an instance only once for all threads
+            std::call_once(init_flag_, []()
+                {
+                    instance_ = std::make_unique<InputManager>(Private());
+                }
+            );
+        }
+        catch ([[maybe_unused]] const std::exception& exception)
+        {
+            throw;
+        }
+            
+        return instance_.get();
     }
 }
--- src/InputModule.h
+++ src/InputModule.h
@@ -12,16 +12,20 @@ namespace InputModule
         InputManager() = default;
 
         static inline std::once_flag init_flag_;
-        static inline std::unique_ptr<InputManager> mng_instance_{ nullptr };
+        static inline std::unique_ptr<InputManager> instance_{ nullptr };
     public:
         InputManager(const InputManager&) = delete;
         InputManager(InputManager&&) = delete;
+        // Use a hack with a private nested struct
+        // to avoid creating objects of this class elsewhere,
+        // but still allow the use of make_unique
         InputManager(Private) {}
         ~InputManager() = default;
 
         InputManager& operator=(const InputManager&) = delete;
         InputManager& operator=(InputManager&&) = delete;
 
+        // Get an instance of a input manager
         static InputManager* get_instance();
     };
 }
